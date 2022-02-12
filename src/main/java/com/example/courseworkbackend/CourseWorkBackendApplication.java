cb063a498f6f0746a74c594d956c478d232857fa
dzyubakanton <nesladkiy@icloud.com>
2022-02-12 14:48:30 +0300

    Initial

---
+++ src/main/java/com/example/courseworkbackend/CourseWorkBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.courseworkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/courseworkbackend/CourseWorkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseworkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
